c264517d697d46a3fc4e170b982a657fd04e7637
Filippo <filippo99merlo@gmail.com>
2024-08-26 11:19:13 +0200

    3rf

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 CACHE_DIR = "/mnt/cimec-storage6/users/filippo.merlo/xai_llama/cache"
 DATASET_PATH = "/mnt/cimec-storage6/users/filippo.merlo/xai_llama/dataset"
-MODEL_PATH = "/mnt/cimec-storage6/users/shared"
+MODEL_PATH = "/mnt/cimec-storage6/users/shared/hf_llms_checkpoints"
 SAVE_IMAGE_PATH = "/mnt/cimec-storage6/users/filippo.merlo/xai_llama/images"
