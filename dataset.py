f80ce404c9f7c88ed8242e05f1b422829931adfd
Filippo <filippo99merlo@gmail.com>
2024-08-27 13:35:30 +0200

    wvf

--- dataset.py
+++ dataset.py
@@ -9,8 +9,9 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=1)
 for _ in range(1):
     example = next(iter(dataloader))
     print(example)
-    #position = example['answers']['answer_start'][0]
-    #context = example["context"][0]
+    position = example['answers']['answer_start'][0]
+    context = example["context"][0]
+    context = context[:position] + "[!!!!!!!!!START!!!!!!!!]" + context[position:]
     #context = context.split()
     #context.insert(position, "[!!!!!!!!!START!!!!!!!!]") 
     #print(context)
