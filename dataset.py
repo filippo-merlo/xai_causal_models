7c3edf59195e7c5eae29bd2e006216d35cd3c021
Filippo <filippo99merlo@gmail.com>
2024-08-27 13:34:01 +0200

    ef

--- dataset.py
+++ dataset.py
@@ -3,16 +3,16 @@ from datasets import load_dataset
 import torch 
 
 # Load the Quoref dataset
-dataset = load_dataset("allenai/quoref", split="validation", cache_dir=DATASET_PATH, trust_remote_code=True)
+dataset = load_dataset("allenai/quoref", split="train", cache_dir=DATASET_PATH, trust_remote_code=True)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=1)
 
 for _ in range(1):
     example = next(iter(dataloader))
     print(example)
-    position = example['answers']['answer_start'][0]
-    context = example["context"][0]
-    context = context.split()
-    context.insert(position, "[!!!!!!!!!START!!!!!!!!]") 
-    print(context)
+    #position = example['answers']['answer_start'][0]
+    #context = example["context"][0]
+    #context = context.split()
+    #context.insert(position, "[!!!!!!!!!START!!!!!!!!]") 
+    #print(context)
 
 
