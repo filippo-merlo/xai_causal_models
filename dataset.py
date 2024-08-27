a2a305f1144cf6afdc4369be2db2f928ffdc9e0b
Filippo <filippo99merlo@gmail.com>
2024-08-27 14:01:43 +0200

    fr

--- dataset.py
+++ dataset.py
@@ -1,19 +1,19 @@
 from config import * 
 from datasets import load_dataset
 import torch 
+import numpy as np
 
 # Load the Quoref dataset
 dataset = load_dataset("allenai/quoref", split="train", cache_dir=DATASET_PATH, trust_remote_code=True)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=1)
 
-for _ in range(1):
+lenghts = []
+for _ in range(dataset.__len__()):
     example = next(iter(dataloader))
-    print(example)
     position = example['answers']['answer_start'][0]
     context = example["context"][0]
-    context = context[:position] + "[!!!!!!!!!START!!!!!!!!]" + context[position:]
-    #context = context.split()
-    #context.insert(position, "[!!!!!!!!!START!!!!!!!!]") 
-    print(context)
+    lenghts.append(context)
+
+print(np.mean(lenghts))
 
 
