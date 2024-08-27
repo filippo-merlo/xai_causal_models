af8cf5e06e18d09f5e331618540855d5bbf5f597
Filippo <filippo99merlo@gmail.com>
2024-08-27 14:15:55 +0200

    t45

--- dataset.py
+++ dataset.py
@@ -5,18 +5,19 @@ import numpy as np
 
 # Load the Quoref dataset
 dataset = load_dataset("allenai/quoref", cache_dir=DATASET_PATH, trust_remote_code=True)
+len_dataset = len(dataset['train'])
 dataset_iter = iter(dataset['train'])
 
 # Get the lenght of the context
 lenghts = []
-for _ in range(10000):
+for _ in range(len_dataset):
     example = next(dataset_iter)
     position = example['answers']['answer_start'][0]
     context = example["context"]
     lenghts.append(len(context))
 # Print the 99th percentile of the lenghts
 print(lenghts)
-print(np.percentile(lenghts, 5))
+print(np.percentile(lenghts, 1))
 
 
 
