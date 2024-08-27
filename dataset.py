3e3c0152f64dbcfb46405af4578f6d96a8c0da43
Filippo <filippo99merlo@gmail.com>
2024-08-27 14:08:43 +0200

    Refactor dataset.py to calculate the 90th percentile of context lengths

--- dataset.py
+++ dataset.py
@@ -15,7 +15,7 @@ for _ in range(dataset.__len__()):
     context = example["context"][0]
     lenghts.append(len(context))
 # Print the 99th percentile of the lenghts
-print(np.percentile(lenghts, 95))
+print(np.percentile(lenghts, 90))
 
 
 
