fe36e58f66b08f7620fe310fa27ec9230b7cb0ad
Filippo <filippo99merlo@gmail.com>
2024-08-27 17:01:12 +0200

    ercv

--- utils.py
+++ utils.py
@@ -4,7 +4,12 @@ from evaluate import load
 
 # exact match
 # predictions (list of str): List of predicted texts.
-# references (list of str): List of reference texts.
+# references (list of str): List of reference texts.ù
+# regexes_to_ignore (list of str): Regex expressions of characters to ignore when calculating the exact matches. Defaults to None. Note: the regex changes are applied before capitalization is normalized.
+# ignore_case (bool): If True, turns everything to lowercase so that capitalization differences are ignored. Defaults to False.
+# ignore_punctuation (bool): If True, removes punctuation before comparing strings. Defaults to False.
+# ignore_numbers (bool): If True, removes all digits before comparing strings. Defaults to False.
+
 exact_match_metric = load("exact_match")
 def exact_match(predictions, references):
     return exact_match_metric.compute(predictions=predictions, references=references)
