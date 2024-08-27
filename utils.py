080f4cf69deed88da2d5aa5a13ad651ec80ab91d
Filippo <filippo99merlo@gmail.com>
2024-08-27 16:45:22 +0200

    weg

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ def exact_match(predictions, references):
 # predictions (list of str): List of predicted texts.
 # references (list of str): List of reference texts.
 def match(predictions, references):
-    return sum([1 for p, r in zip(predictions, references) if p in r]) / len(references)
+    return sum([1 for p, r in zip(predictions, references) if r in p]) / len(references)
 
 # f1 score
 # predictions (list of int): Predicted labels.
