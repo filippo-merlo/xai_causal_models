787ae18857b30f6bf2b31899b4bdc56251a11e90
Filippo <filippo99merlo@gmail.com>
2024-08-28 17:55:47 +0200

    efv

--- explain_llama3.py
+++ explain_llama3.py
@@ -84,8 +84,7 @@ for idx, example in enumerate(DATASET_SHORT[:]):
     model.eval()
     with torch.no_grad():
         output_ids = model.generate(
-            model_input, max_new_tokens=256,
-            eos_token_id=terminators)[0]
+            model_input, max_new_tokens=256)[0]
         response = tokenizer.decode(output_ids, skip_special_tokens=True)
         print(response)
         response = response.split('\nAnswer:')[-1]
